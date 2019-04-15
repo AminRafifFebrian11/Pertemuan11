ee8c5465af7a718490b338a18b172d055b9e03d9
AminRafifFebrian11 <49119429+AminRafifFebrian11@users.noreply.github.com>
2019-04-15 18:53:39 +0700

    penambahan script garis pada baris 1 sampai 4

--- halojakarta.cpp
+++ halojakarta.cpp
@@ -1,9 +1,13 @@
-#include <escream>
-using namespes std;
+#include <iostream>
+using namespace std;
+#include <iostream>
+using namespace std;
 
 int main()
 {
     int halo="Halo jakarta!";
     count<< halo <<endl;
+    string halo="Halo jakarta!";
+    cout<< halo <<endl;
     return 0;
 }
